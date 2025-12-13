3a3abf334f7a4d058c3ec2839fc6159ad8acf480
William <william.moy@cobi.tech>
2025-12-13 14:17:46 -0600

    updated the benchmarking utils and test to work with the new backend formatting

--- pita/tests/benchmarking_test.py
+++ pita/tests/benchmarking_test.py
@@ -1,6 +1,6 @@
 # Training Free Reasoning Libraries
-from pita.utils.benchmarking_utils import benchmark_sampling
-from pita.inference.autoregressive_sampler_backend import create_autoregressive_sampler
+from pita.utils.benchmarking_utils import benchmark_sampling, load_benchmark
+from pita.inference.LLM_backend import create_autoregressive_sampler
 from pita.sampling.power_sample import enable_power_sampling
 
 # Pytorch Library
@@ -13,34 +13,34 @@ import time
 # Main function to test power sampling
 if __name__ == "__main__":
 
-
     # Initialize the random number generator
     #seed = 42
     seed = time.time_ns() % (2**32 - 1)
     torch.manual_seed(seed)
     random.seed(seed)
 
-    # Power Sampling Hyperparameters
-    total_output_tokens = 1000 #total tokens for response
-    block_size = 250 # tokens per block. Number of blocks = token_count / block_size
-    MCMC_steps = 5 
-
     engine_name = "vllm"
 
     # LLM parameters
     if(engine_name == "vllm"):
-        model_name = "Qwen/Qwen3-4B-AWQ"
-        dtype = "auto"
-        tokenizer_path = None
-        max_logprobs = 100
-        logits_per_token = 100
+        _model_name = "Qwen/Qwen3-4B-AWQ"
+        _dtype = "auto"
+        _tokenizer_path = None
+        _gpu_memory_utilization = 0.25
+        _max_model_len = 2048
+        _max_logprobs = 2
+        _logits_per_token = 2
+        _normalization_constant = True
 
     elif(engine_name == "llama_cpp"):
-        model_name = "unsloth/Qwen3-4B-Instruct-2507-GGUF"
-        dtype = "Q5_K_M"
-        tokenizer_path="Qwen/Qwen3-4B-AWQ"
-        max_logprobs = None
-        logits_per_token = 1000
+        _model_name = "unsloth/Qwen3-4B-Instruct-2507-GGUF"
+        _dtype = "Q5_K_M"
+        _tokenizer_path="Qwen/Qwen3-4B-Instruct-2507"
+        _gpu_memory_utilization = 0.85
+        _max_model_len = 2048
+        _max_logprobs = None
+        _logits_per_token = 100
+        _normalization_constant = True
         
         # Tell Pytorch to use the GPU if available
         if torch.cuda.is_available():
@@ -48,45 +48,53 @@ if __name__ == "__main__":
         else:
             device = torch.device("cpu")
 
-    gpu_memory_utilization = 0.8
-    max_model_len = 8192
-
-    #Initialize Autoregressive Sampler
-    sampler = create_autoregressive_sampler(
+    #Initialize LLM 
+    llm = create_autoregressive_sampler(
         engine=engine_name, 
-        model=model_name, 
-        dtype=dtype,
-        tokenizer_path="Qwen/Qwen3-4B-AWQ", 
-        gpu_memory_utilization=gpu_memory_utilization, 
-        max_model_len=max_model_len, 
-        max_logprobs = max_logprobs,
-        logits_per_token = logits_per_token
+        model=_model_name, 
+        dtype=_dtype,
+        tokenizer_path=_tokenizer_path, 
+        gpu_memory_utilization=_gpu_memory_utilization, 
+        max_model_len=_max_model_len, 
+        max_logprobs = _max_logprobs,
+        logits_per_token = _logits_per_token,
+        normalization_constants = _normalization_constant
     )
 
-    # Create the power sampling parameters to use
-    enable_power_sampling(sampler, total_output_tokens, block_size, MCMC_steps)
+    # Enable Power Sampling
+    # Set sampling parameters
+    llm.sampling_params.max_tokens = 1000
+    llm.sampling_params.temperature = 0.25
 
-    # Test MATH500 Benchmark
-    dataset_name = "AIME"
-    power_sampling_on = True
-    power_sampling_windowed_on = False
-    low_temp_sampling_on = False
-    naive_sampling_on = False
-    chain_of_thought = False
-    #for temp in [0.25, 0.5, 0.75]:
-    for temp in [0.5]:
-        sampler.sampling_params.temperature = temp
-        benchmark_sampling(
-            dataset_name, 
-            sampler, 
-            chain_of_thought, 
-            power_sampling_on, 
-            power_sampling_windowed_on, 
-            low_temp_sampling_on, 
-            naive_sampling_on, 
-            question_max = 1, 
-            output_file_name = f"results/{dataset_name}_power_sampling_results_temp_{temp}.csv", 
-            seed=seed
-        )
+    # Power Sampling Hyperparameters
+    block_size = 200 # tokens per block. Number of blocks = token_count / block_size
+    MCMC_steps = 10
 
-    del sampler
+    # Enable Power Sampling
+    enable_power_sampling(
+        llm,
+        block_size, # tokens per block. Number of blocks = token_count / block_size
+        MCMC_steps, # MCMC steps per block
+    )
+
+    # Load dataset to test
+    system_message, question_list, answer_list = load_benchmark("MATH500")
+    # Define sampling techniques to benchmark
+    sampling_techniques = [True, True, True, False, False] # temp=1 sampling, low temp sampling, power sampling, smc, best of n
+    # Define the **kwargs for the benchmark
+    kwargs = {
+    "power_sampling_logging": True,
+    "power_sampling_logging_path": "results/power_sampling_logs"
+    }
+    # Run the benchmark
+    benchmark_sampling(
+        llm=llm,
+        system_message=system_message,
+        question_list=question_list,
+        answer_list=answer_list,
+        enable_thinking=False, 
+        sampling_techniques=sampling_techniques, 
+        max_questions=10, 
+        output_file_name="results/math500_power_sampling_results.csv",
+        **kwargs
+    )
