5b6f85e24db21452550f1fa86512fe25b7d0e93a
William Moy <william.moy@cobi.tech>
2025-12-29 06:21:25 -0600

    Update power_sampling.py comments
    updated the comments to be more descriptive

--- pita/sampling/power_sample.py
+++ pita/sampling/power_sample.py
@@ -45,7 +45,7 @@ class Power_Sampling:
     Power Sampling Class that stores the parameters and methods used for power sampling.
     
     Attributes:
-        block_size (int): How many blocks to divide the total output tokens into for power sampling. Smaller block sizes = better quality but slower
+        block_size (int): How many tokens to divide the total output tokens into for power sampling. number of blocks = (sampler.sampling_params.max_tokens)/block_size. Smaller block sizes = better quality but slower
         MCMC_steps (int): Number of MCMC steps to perform per block. More steps = better quality but slower
         token_metric (str): Metric to use for token selection. Can be "logprobs", "power_distribution", or "entropy"
     """
